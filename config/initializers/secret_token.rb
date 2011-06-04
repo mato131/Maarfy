5247de201559801e9b526990bf3fa6814b64bb33
Mohamed Loutfi <mato131@gmail.com>
2011-06-04 03:11:41 +0100

    first comit3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maarfy::Application.config.secret_token = '87d62602e00107c371396982c2ad629d21eb41ab7c13aeeebcfce0d447d15ac4113ffdba6481fe78b83c526c208e05edb3e26fe24db450e772960d3e005f073f'
